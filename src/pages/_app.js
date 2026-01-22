4daf03de9395b5351fc9b4ed40c60aea2e76c9e0
Bidhyashree <bidhyashree.dahal@dcmail.ca>
2026-01-22 09:27:39 -0500

    Refresh typography

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,12 +3,27 @@ import "../styles/globals.css";
 import Navbar from "../components/NavBar";
 import Footer from "../components/Footer";
 import { CartProvider } from "../components/CartContext";
+import { Inter, Playfair_Display } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+  display: "swap",
+});
+
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-display",
+  display: "swap",
+});
 export default function MyApp({ Component, pageProps }) {
   return (
     <CartProvider>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
+      <div className={`${inter.variable} ${playfair.variable}`}>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
     </CartProvider>
   );
 }
